7f51ce4ced85e3cdec8ca7b3b6204031a9fed1dd
Romel Mendoza <romel.mendoza@stellari.ca>
2025-11-21 11:43:43 -0700

    Fix frontend styling

--- code-house-sorting-quiz/src/components/HousePopup.jsx
+++ code-house-sorting-quiz/src/components/HousePopup.jsx
@@ -1,18 +1,4 @@
-import Popup from "reactjs-popup"
-import 'reactjs-popup/dist/index.css'
 
-const CENTER = "center center"
-
-const InfoStyle = {
-    padding: '20px',
-    textAlign: 'center',
-    width: "600px",
-    height: "400px",
-    backgroundColor: "grey",
-    borderRadius: "20px",
-}
-
-//How to minimize this styling?
 const PantherStyle = {
     width: "200px",
     height: "200px",
@@ -47,28 +33,19 @@ const SaberStyle = {
     borderRadius: "50%",
     boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
     clipPath: "circle(50%)",
-}
+} 
 
-export const HousePopup = ({icon, color, description, alt, name, num}) => {
+export const HousePopup = ({icon, alt, name, num}) => {
 
     const imageStyles = [PantherStyle, BatStyle, LynxStyle, SaberStyle]
 
     return (
         <div className="flex flex-col gap-4 ">
-            <label className="font-bold text-shadow-lg">{name}</label>
-            { /*<Popup 
-                trigger={
-                    <button className="rounded-full">
-                        <img src={icon} style={imageStyles[num]} alt={alt} />
-                    </button>} 
-                position={CENTER}
-                contentStyle={InfoStyle}
-            >
-                <div className="flex flex-col text-center ">
-                    <h1 className="text-2xl underline decoration-4 mb-4">{name}</h1>
-                    <p>{description}</p>
-                </div>
-            </Popup> */ }
+            <div className="relative fit-content ">
+                <label className="font-bold text-shadow-lg backdrop-blur-sm">{name}</label>
+            </div>
+            
+            <img src={icon} style={imageStyles[num]} alt={alt} className="backdrop-blur-sm" />
         </div>
     )
 }
